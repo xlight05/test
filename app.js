b1bb99a5a13aa4494fc31749be982a9b424a01a4
xLight <anjanasupun05@gmail.com>
2017-09-30 14:54:17 +0530

    hiii

---
+++ app.js
@@ -0,0 +1,12 @@
+const axios  = require('axios');
+
+axios.defaults.headers.post['Ocp-Apim-Subscription-Key'] = 'f6b55b4ff4214bf6b28ba29e72e34a82';
+
+let endPoint = 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?';
+axios.post (endPoint,{
+    url:"http://hd.wallpaperswide.com/thumbs/dr_house_hugh_laurie-t2.jpg"
+}).then((res)=>{
+    console.log(res.data)
+}).catch((err)=>{
+    console.log(err);
+})
